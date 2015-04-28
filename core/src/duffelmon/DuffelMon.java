bf7c0e33f7611175a0a51f78e6714faed6c96264
jkutaso <jkutasov@gmail.com>
2015-04-28 13:35:15 -0500

    Write health on screen

--- core/src/duffelmon/DuffelMon.java
+++ core/src/duffelmon/DuffelMon.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Color;
 
 public class DuffelMon extends ApplicationAdapter {
 	SpriteBatch batch;
