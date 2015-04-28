cb1251df6fac847276b402d4ab277b8cd04c5714
NickGupta <nickmgupta@gmail.com>
2015-04-28 14:14:46 -0500

    Created a second dufflemon for the battle

--- core/src/duffelmon/DuffelMon.java
+++ core/src/duffelmon/DuffelMon.java
@@ -13,10 +13,17 @@ public class DuffelMon extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
         BitmapFont font;
+        
+        // creates the first dufflemon for the battle
         Species crabmon = new Species("CrabMon");
-        Stats stat = new Stats(1, 0, 10, 3, 10, 25);
-        Mon player = new Mon("Bob", crabmon, stat);
-	
+        Stats crabmonStat = new Stats(1, 0, 10, 3, 10, 25);
+        Mon player = new Mon("Bob", crabmon, crabmonStat);
+        
+	// creates the second difflemon for the battle
+        Species geobro = new Species("Geobro");
+        Stats geobroStat = new Stats(1, 0, 10, 3, 10, 25);
+        Mon enemy = new Mon("Joe", geobro, geobroStat);
+        
         @Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -26,14 +33,17 @@ public class DuffelMon extends ApplicationAdapter {
         
 	@Override
 	public void render () {
-                long health = Math.round(player.getHealth());
-                long maxHealth = Math.round(player.getMaxHealth());
+                long playerHealth = Math.round(player.getHealth());
+                long playerMaxHealth = Math.round(player.getMaxHealth());
+                long enemyHealth = Math.round(enemy.getHealth());
+                long enemyMaxHealth = Math.round(enemy.getMaxHealth());
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		//batch.draw(img, 0, 0);
                 font.setColor(Color.WHITE);
-                font.draw(batch, "Player health: " + health + " / " + maxHealth, 300, 300);
+                font.draw(batch, "Player health: " + playerHealth + " / " + playerMaxHealth, 300, 300);
+                font.draw(batch, "Enemy health: " + enemyHealth + " / " + enemyMaxHealth, 300, 250);
 		batch.end();
                
                 
