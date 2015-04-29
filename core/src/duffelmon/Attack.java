2c5132be4a3b64ae630f1fa31bae57ca4c218a9e
jkutaso <jkutasov@gmail.com>
2015-04-29 13:27:46 -0500

    Creates attack class

--- core/src/duffelmon/Attack.java
+++ core/src/duffelmon/Attack.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package duffelmon;
-
+import java.util.*;
 /**
  *
  * @author csstudent
  */
 public class Attack {
+    private static TreeMap<String,Attack> map = new TreeMap<String,Attack>();
+    private String name;
+    private Attack(String n){
+        name = n;
+    }
     
+    public static Attack makeAttack(String n) {
+        Attack a = new Attack(n);
+        map.put(n, a);
+        return a;
+    }
+    
+    public static Attack getAttack(String s) {
+        return map.get(s);
+    }
+    
+    public String getName() {
+        return name;
+    }
 }
