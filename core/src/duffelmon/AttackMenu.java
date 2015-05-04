53319ac2440abf8ab844c610677e13dbfa8ef367
jkutaso <jkutasov@gmail.com>
2015-05-04 13:22:25 -0500

    creates placeholder methods for pushing buttons

---
+++ core/src/duffelmon/AttackMenu.java
@@ -0,0 +1,35 @@
+/*
+*Methods that return booleans to determine if a key is being pushed 
+ */
+package duffelmon;
+
+/**
+ *
+ * @author csstudent
+ */
+public class AttackMenu {
+    
+    public AttackMenu(){
+        
+    }
+    
+    public boolean upIsPushed(){
+        return true;
+    
+    }
+    
+    public boolean downIsPushed(){
+        return true;
+    
+    }
+    
+    public boolean leftIsPushed(){
+        return true;
+    
+    }
+    
+    public boolean rightIsPushed(){
+        return true;
+    
+    }
+}
