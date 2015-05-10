25b5ec99c4d9251dd3e3901521cddc4ea5e49080
742mph <abheyman23@sbcglobal.net>
2015-05-10 13:03:30 -0500

    Set window size to 512x448

--- desktop/src/duffelmon/desktop/DesktopLauncher.java
+++ desktop/src/duffelmon/desktop/DesktopLauncher.java
@@ -1,12 +1,13 @@
 package duffelmon.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+//import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import duffelmon.DuffelMon;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new DuffelMon(), config);
+		//LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//new LwjglApplication(new DuffelMon(), config);
+                new LwjglApplication(new DuffelMon(), "DuffelMon", 512, 448);
 	}
 }
