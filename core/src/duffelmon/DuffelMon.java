897450bf77035f619bfc34951b8e8a6f98f171f7
NickGupta <nickmgupta@gmail.com>
2015-05-19 14:12:32 -0500

    created a lot of different moves with filler for damage

--- core/src/duffelmon/DuffelMon.java
+++ core/src/duffelmon/DuffelMon.java
@@ -59,11 +59,11 @@ public class DuffelMon extends ApplicationAdapter {
             Type bug = Type.makeType("Bug");
             
             //Type effectiveness
-            fire.addRelationship(fire, 0.5);
+            fire.addRelationship(fire, 0);
             fire.addRelationship(water, 2);
             fire.addRelationship(ice, 2);
             fire.addRelationship(air, 2);
-            fire.addRelationship(electric, 0.5);
+            fire.addRelationship(electric, 0);
             fire.addRelationship(plant, 0.5);
             water.addRelationship(fire, 0.5);
             water.addRelationship(electric, 2);
@@ -97,13 +97,14 @@ public class DuffelMon extends ApplicationAdapter {
                 @Override
                 public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
                     switch(step) {
+                        /*
                         case 0:
                             uDisplay.setXSpeed(4);
                             nextMoveStep(uDisplay);
                             waitUntilNextMoveStep(uDisplay, 30);
                             break;
                         case 1:
-                            absoluteDamage(uDisplay, tDisplay, getDamage());
+                            absoluteDamage(tDisplay, getDamage());
                             uDisplay.setXSpeed(-4);
                             nextMoveStep(uDisplay);
                             waitUntilNextMoveStep(uDisplay, 30);
@@ -112,10 +113,236 @@ public class DuffelMon extends ApplicationAdapter {
                             uDisplay.setXSpeed(0);
                             finishMove(uDisplay);
                             break;
+                        */
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
                     }
                 }
             });
             Move spark = Move.makeMove(new Move("Spark", fire, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            Move shock = Move.makeMove(new Move("Shock", electric, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // Should slow the speed of the mon hit and do a small amount of damage aswell
+            Move coldTouch = Move.makeMove(new Move("Cold Touch", ice, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            Move snowBall = Move.makeMove(new Move("Snow Ball", ice, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // steadys the mon on the ground preparing to be hit, raises defence
+            Move steady = Move.makeMove(new Move("Steady", earth, false, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            Move gust = Move.makeMove(new Move("Gust", air, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // does poisen type damage but has no chance of applying a DOT effect
+            Move sting = Move.makeMove(new Move("Sting", poison, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // does a little but of damage, but has a high chance of applying a DOT
+            Move poisonDart = Move.makeMove(new Move("Poison Dart", poison, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // does dmg and has a small chance of slowing the targets speed
+            Move iceBlast = Move.makeMove(new Move("Ice Blast", ice, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // blows a very cold gust of wind at the target
+            // does little damage, but has a very high chance to slow speed and damage
+            Move northernWinds = Move.makeMove(new Move("Northern Winds", ice, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // covers the mon in a layer of string ice to raise defence
+            Move iceShield = Move.makeMove(new Move("Ice Shield", ice, false, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            Move inferno = Move.makeMove(new Move("Inferno", fire, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // the mons thick skin to defend against the fire they use to
+            // attack also helps defend against other things raises defence
+            Move thickSkin = Move.makeMove(new Move("Thick Skin", fire, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            Move thunderBolt = Move.makeMove(new Move("Thunder Bolt", electric, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            Move lightningStrike = Move.makeMove(new Move("Lightning Strike", electric, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // raises the attack of the dufflemon
+            Move overCharge = Move.makeMove(new Move("Over Charge", electric, false, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            Move waterGun = Move.makeMove(new Move("Water Gun", water, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            Move waterBlast = Move.makeMove(new Move("Water Blast", water, true, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            // dufflemon hydrates themself, gaining stats
+            Move hydrate = Move.makeMove(new Move("Hydrate", water, false, 25, 1, 35, 0) {
+                @Override
+                public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
+                    switch(step) {
+                        case 0:
+                            basicDamageAttempt(uDisplay, tDisplay);
+                            finishMove(uDisplay);
+                            break;
+                    }
+                }
+            });
+            Move rockThrow = Move.makeMove(new Move("Rock Throw", earth, true, 25, 1, 35, 0) {
                 @Override
                 public void doMoveStep(MonDisplay uDisplay, MonDisplay tDisplay, int step) {
                     switch(step) {
