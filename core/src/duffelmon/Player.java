fc49df00f820721dfbccf3c6a93dc91c4c905c71
742mph <abheyman23@sbcglobal.net>
2015-06-07 18:25:08 -0500

    Storage arrays for mons and items

--- core/src/duffelmon/Player.java
+++ core/src/duffelmon/Player.java
@@ -11,8 +11,8 @@ package duffelmon;
  */
 public class Player extends Trainer {
     
-    private Mon[] mons;
-    private Item[] items;
+    private Mon[] monsInStorage = new Mon[50];
+    private Item[] itemsInStorage = new Item[50];
     private int money;
     
     public Player(String n) {
@@ -22,6 +22,22 @@ public class Player extends Trainer {
     
     public int getMoney() {
         return money;
+    }
+    
+    public Mon[] getStorageMons() {
+        return monsInStorage;
+    }
+    
+    public Mon getStorageMon(int pos) {
+        return monsInStorage[pos];
+    }
+    
+    public Item[] getStorageItems() {
+        return itemsInStorage;
+    }
+    
+    public Item getStorageItem(int pos) {
+        return itemsInStorage[pos];
     }
     
     public int giveMoney(int m) {
@@ -36,8 +52,41 @@ public class Player extends Trainer {
         return taken;
     }
     
+    public Mon takeStorageMon(int pos) {
+        Mon m = monsInStorage[pos];
+        monsInStorage[pos] = null;
+        return m;
+    }
+    
+    public boolean addStorageMon(Mon m) {
+        for(int i = 0; i < monsInStorage.length; i++) {
+            if (monsInStorage[i] == null) {
+                monsInStorage[i] = m;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Item takeStorageItem(int pos) {
+        Item i = itemsInStorage[pos];
+        itemsInStorage[pos] = null;
+        return i;
+    }
+    
+    public boolean addStorageItem(Item it) {
+        for(int i = 0; i < itemsInStorage.length; i++) {
+            if (itemsInStorage[i] == null) {
+                itemsInStorage[i] = it;
+                return true;
+            }
+        }
+        return false;
+    }
+    
     @Override
     public int getMoneyToGive() {
         return money/2;
     }
+    
 }
